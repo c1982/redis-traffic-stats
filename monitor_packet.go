b808065f0a9172c8654f8d496b6e834f7df413f3
c1982 <aspsrc@gmail.com>
2021-01-24 18:38:19 +0300

    refactor: changed snap lenght for  performance

--- monitor_packet.go
+++ monitor_packet.go
@@ -12,7 +12,7 @@ import (
 //StartMonitor monitor redis packet destination or source port
 func StartMonitor(devicename string, redisport uint16) error {
 	bpffilter := fmt.Sprintf("dst port %d and tcp[((tcp[12:1] & 0xf0) >> 2):1] = 0x2A", redisport)
-	handle, err := pcap.OpenLive(devicename, 65535, false, -1*time.Second)
+	handle, err := pcap.OpenLive(devicename, 1600, false, -1*time.Second)
 	if err != nil {
 		return fmt.Errorf("error opening device %s: %v", devicename, err)
 	}
